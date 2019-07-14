37c599ed5ef8e6adbc13d9eb766f3e9f3d5ecaf9
Kevin Krato <kratok08@gmail.com>
2019-07-13 18:30:25 -0600

    Add updateQuality method

--- Idea.js
+++ Idea.js
@@ -2,6 +2,10 @@ class Idea {
   constructor(title, body) {
     this.title = title;
     this.body = body;
+    this.quality = "Swill";
     this.id = Date.now();
+  }
+  updateQuality(quality) {
+    this.quality(quality);
   }
 }
